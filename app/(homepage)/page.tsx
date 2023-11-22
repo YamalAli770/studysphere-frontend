7c6e45c6dce918704d3aade541757b018f943f28
YamalAli770 <yamalali770@gmail.com>
2023-11-22 19:05:16 +0500

    created meetup section

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
 import Hero from './_components/hero'
+import Meetup from './_components/meetup'
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Meetup />
     </div>
   )
 }

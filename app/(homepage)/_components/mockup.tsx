9e46fbc16ced5ad27a4dd7f60ce4d3c9dc7b0ab2
YamalAli770 <yamalali770@gmail.com>
2023-11-22 21:05:36 +0500

    created mockup section

---
+++ app/(homepage)/_components/mockup.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Mockup() {
+  return (
+    <div className='py-12'>
+        <div className="container">
+            <div className='flex flex-col gap-y-24'>
+                {/* Top */}
+                <section className="flex justify-between">
+                    <h1 className="text-4xl">
+                        Join The Conversation,<br /> Share Experiences
+                    </h1>
+                    <p className="text-sm w-72">
+                        Engage in our collaborative forum where knowledge knows no borders. Join discussions, seek advice, and share your study abroad journey with a supportive community of global learners.
+                    </p>
+                </section>
+                {/* Bottom */}
+                <section className='flex'>
+                    <div className='relative w-full h-96'>
+                        <Image src="/mockup-1.png" alt='mobile-mockup' fill={true} style={{objectFit: 'contain'}} />
+                    </div>
+                    <div className='relative w-full h-96'>
+                        <Image src="/mockup-2.png" alt='mobile-mockup' fill={true} style={{objectFit: 'contain'}} />
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+  )
+}
--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,11 +1,13 @@
 import Hero from './_components/hero'
 import Meetup from './_components/meetup'
+import Mockup from './_components/mockup'
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Meetup />
+      <Mockup />
     </div>
   )
 }

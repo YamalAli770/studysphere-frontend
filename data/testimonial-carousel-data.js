6ec5085e257cd273e7209f1bd9394c992ad827c0
YamalAli770 <yamalali770@gmail.com>
2023-11-23 17:13:33 +0500

    changed testimonial images

--- data/testimonial-carousel-data.js
+++ data/testimonial-carousel-data.js
@@ -1,26 +1,26 @@
 export const testimonialCarouselData = [
     {
-        src: "/slider/slider-1.jpg",
-        name: "Yamal Ali",
-        handle: "@yamalali",
+        src: "/testimonial/testimonial-1.jpg",
+        name: "Aisha Khan",
+        handle: "@aisha_khan",
         testimony: "StudySphere has revolutionized how I connect with mentees. The platform's intuitive design and diverse community make mentorship meaningful and enjoyable. It's a privilege to guide the next generation of global learners."
     },
     {
-        src: "/slider/slider-2.jpg",
+        src: "/testimonial/testimonial-2.jpg",
         name: "John Wolf",
         handle: "@john_wolf",
         testimony: "I've been a mentor on StudySphere for over a year now, and I can't imagine my life without it. I've met so many amazing people, and I've learned so much about myself. I'm so grateful for this platform."
     },
     {
-        src: "/slider/slider-3.jpg",
-        name: "Aisha Khan",
-        handle: "@aisha_khan",
+        src: "/testimonial/testimonial-3.jpg",
+        name: "Alexa Smith",
+        handle: "@alexa_smith",
         testimony: "Finding StudySphere was a blessing. The mentors genuinely care, and the resources are a goldmine of information. This app has made my study abroad journey smoother and more enjoyable."
     },
     {
-        src: "/slider/slider-7.jpg",
-        name: "Lee Min Ho",
-        handle: "@aisha_khan",
+        src: "/testimonial/testimonial-4.jpg",
+        name: "Joshua Lee",
+        handle: "@joshy_lee",
         testimony: "Finding StudySphere was a blessing. The mentors genuinely care, and the resources are a goldmine of information. This app has made my study abroad journey smoother and more enjoyable."
     }
 ];

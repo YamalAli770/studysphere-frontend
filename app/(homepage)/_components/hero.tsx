2c079391bd5613c0467bb936bc87c8badde086bb
YamalAli770 <yamalali770@gmail.com>
2023-11-23 11:06:18 +0500

    refactored hero section

--- app/(homepage)/_components/hero.tsx
+++ app/(homepage)/_components/hero.tsx
@@ -2,7 +2,30 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { heroData } from '@/data/hero-data'
+
+export const heroData = [
+    {
+        src: '/hero/hero-image-1.jpg',
+        className: 'relative w-60 h-72',
+    },
+    {
+        src: '/hero/hero-image-2.jpg',
+        className: 'relative w-56 h-60',
+    },
+    {
+        src: '/hero/hero-image-3.jpg',
+        className: 'relative w-72 h-44',
+    },
+    {
+        src: '/hero/hero-image-4.jpg',
+        className: 'relative w-56 h-60',
+    },
+    {
+        src: '/hero/hero-image-5.jpg',
+        className: 'relative w-60 h-72',
+    },
+];
+  
 
 export default function Hero() {
   return (

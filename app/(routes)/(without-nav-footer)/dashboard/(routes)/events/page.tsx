b92e5a047c182914029699a6cc5e24e5234f9d72
001osama <osamanasim909@gmail.com>
2023-12-24 21:39:45 +0500

    add pagination in page

--- app/(routes)/(without-nav-footer)/dashboard/(routes)/events/page.tsx
+++ app/(routes)/(without-nav-footer)/dashboard/(routes)/events/page.tsx
@@ -1,20 +1,16 @@
 "use client"
 
 import * as React from "react";
-import { Calendar } from "@/components/ui/calendar";
 import EventCard from "./_components/event-card";
-import { Button } from '@/components/ui/button';
 import { DateFilter } from "./_components/date-filter";
+import PaginationBar from "./_components/pagination-bar";
 
 
 
 export default function Events(){
-    
-    
-    
     return(
         <div className='p-8'>
-            <div className="flex justify-between mb-12">
+            <div className="flex justify-between">
                 <div>
                     <div className='text-3xl font-semibold'>
                         Upcoming Meetings
@@ -27,7 +23,7 @@ export default function Events(){
                     <DateFilter></DateFilter>
                 </div>
             </div>
-            <div className="grid gap-x-8 gap-y-10 lg:gap-y-12 grid-cols-2 grid-rows-3 lg:grid-cols-3 lg:grid-rows-2">
+            <div className="my-12 grid gap-x-8 gap-y-10 lg:gap-y-12 grid-cols-2 grid-rows-3 lg:grid-cols-3 lg:grid-rows-2">
                 <EventCard></EventCard>
                 <EventCard></EventCard>
                 <EventCard></EventCard>
@@ -35,8 +31,8 @@ export default function Events(){
                 <EventCard></EventCard>
                 <EventCard></EventCard>
             </div>
-            <div>
-
+            <div className="border-t-2 py-6">
+                <PaginationBar />
             </div>
         </div>
     )

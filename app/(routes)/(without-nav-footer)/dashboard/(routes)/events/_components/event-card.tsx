ef02bb0ae72f2df8caadd57b4fdc38ae3199a9c5
001osama <osamanasim909@gmail.com>
2023-12-24 22:12:45 +0500

    made event card take dynamic data

--- app/(routes)/(without-nav-footer)/dashboard/(routes)/events/_components/event-card.tsx
+++ app/(routes)/(without-nav-footer)/dashboard/(routes)/events/_components/event-card.tsx
@@ -2,17 +2,26 @@ import Image from 'next/image';
 import { Badge } from "@/components/ui/badge";
 import { Button } from '@/components/ui/button';
 
-export default function EventCard(){
+type Event = {
+    time: string;
+    date: string;
+    profileImageUrl: string;
+    attendeeName: string;
+    duration: number;
+    cost: number;
+  };
+
+export default function EventCard(props:Event){
     return(
         <div className="rounded-xl px-5 bg-gray-100/70 hover:bg-white hover:shadow-xl duration-500">
             <div className="flex py-4 border-b justify-between items-center">
                 <div>
-                    <p>10:00 - 11:00 AM</p>
-                    <p className='text-gray-400 text-sm'>Monday, August 19</p>
+                    <p>{props.time}</p>
+                    <p className='text-gray-400 text-sm'>{props.date}</p>
                 </div>
                 <div className="rounded-full relative overflow-hidden h-12 w-12">
                     <Image
-                    src={"https://ucarecdn.com/5a2e1064-794b-413e-866d-5f8ade379174/-/preview/500x500/-/quality/smart_retina/-/format/auto/"}
+                    src={props.profileImageUrl}
                     fill={true}
                     style={{objectFit: "cover"}}
                     alt="profile-photo">
@@ -21,11 +30,11 @@ export default function EventCard(){
             </div>
             <div className='flex py-4 border-b justify-between items-center'>
                 <div className='flex items-center space-x-2'>
-                    <p>Yamal Ali</p>
-                    <Badge className='cursor-default select-none'>1 &nbsp; hour</Badge>
+                    <p>{props.attendeeName}</p>
+                    <Badge className='cursor-default select-none'>{props.duration} &nbsp; hour</Badge>
                 </div>
                 <div className='text-accent-text'>
-                    $ 80.00
+                    $ {props.cost}
                 </div>
             </div>
             <div className='flex space-x-5 py-4 justify-center'>

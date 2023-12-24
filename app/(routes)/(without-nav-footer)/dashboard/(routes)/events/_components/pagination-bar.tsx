e4762b946eecfc17c7dfc481cef2d622b4380108
001osama <osamanasim909@gmail.com>
2023-12-24 22:12:30 +0500

    used pagination comp of shade cn lib in pagination bar

--- app/(routes)/(without-nav-footer)/dashboard/(routes)/events/_components/pagination-bar.tsx
+++ app/(routes)/(without-nav-footer)/dashboard/(routes)/events/_components/pagination-bar.tsx
@@ -1,6 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/components/ui/pagination"
+
 export default function PaginationBar() {
   return (
     <div className="flex justify-between">
@@ -11,18 +21,29 @@ export default function PaginationBar() {
             <p>Page <span>1/3</span></p>
         </section>
         {/* Right */}
-        <section className="flex bg-primary rounded-md">
-            <Button variant="pagination">
-                <ChevronLeft />
-            </Button>
-            <div className="flex">
-                <Button variant="pagination">1</Button> 
-                <Button variant="pagination">2</Button> 
-                <Button variant="pagination">3</Button> 
-            </div>
-            <Button variant="pagination">
-                <ChevronRight />
-            </Button>
+        <section>
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">2</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">3</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </section>
     </div>
   )

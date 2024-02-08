340e1b8cf505a4454c193ed7c30eb513a9a257dc
YamalAli770 <yamalali770@gmail.com>
2024-02-08 21:59:52 +0500

    refactored project structure

---
+++ app/(routes)/onboarding/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function OnBoarding() {
+  return (
+    <div>
+        {/* Top */}
+        <section className='p-6'>
+            <h2>Study Sphere</h2>
+        </section>
+    </div>
+  )
+}

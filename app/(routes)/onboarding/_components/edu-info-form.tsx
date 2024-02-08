0df9452ca9f237d66b9dccc1db68f2ebe57516f5
YamalAli770 <yamalali770@gmail.com>
2024-02-09 00:34:27 +0500

    created onboarding routes

---
+++ app/(routes)/onboarding/_components/edu-info-form.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EducationalInformationForm() {
+  return (
+    <div>EducationalInformationForm</div>
+  )
+}
--- app/(routes)/onboarding/page.tsx
+++ app/(routes)/onboarding/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import { PiNumberCircleFiveBold, PiNumberCircleFourBold, PiNumberCircleOneBold, PiNumberCircleSixBold, PiNumberCircleThreeBold, PiNumberCircleTwoBold } from "react-icons/pi";
 
 export default function OnBoarding() {
   return (
     <div>
         {/* Top */}
         <section className='p-6'>
-            <h2>Study Sphere</h2>
+            <h2 className="text-4xl">Study <span className="text-accent-text">Sphere</span></h2>
+        </section>
+        {/* Bottom */}
+        <section className="grid grid-cols-7 p-6">
+            {/* Tabs */}
+            <div className="col-span-1">
+                <ul>
+                    <li className="flex items-center gap-2 text-lg">
+                        <PiNumberCircleOneBold />
+                        <p>First Step</p>
+                    </li>
+                    <div className="border-l-4 h-11"></div>
+                    <li className="flex items-center gap-2 text-lg">
+                        <PiNumberCircleTwoBold />
+                        <p>Second Step</p>
+                    </li>
+                    <div className="border-l-4 h-11"></div>
+                    <li className="flex items-center gap-2 text-lg">
+                        <PiNumberCircleThreeBold />
+                        <p>Third Step</p>
+                    </li>
+                    <div className="border-l-4 h-11"></div>
+                    <li className="flex items-center gap-2 text-lg">
+                        <PiNumberCircleFourBold />
+                        <p>Fourth Step</p>
+                    </li>
+                </ul>
+            </div>
+            {/* Tabs Content */}
+            <div className="col-span-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, quos aut sapiente illum, odit dolor dolorum suscipit nihil voluptate enim qui nam asperiores similique tenetur maiores libero obcaecati voluptatum? Dignissimos quo ea voluptas id ipsa impedit quasi deserunt eveniet laboriosam temporibus exercitationem, nihil vel cum doloremque voluptatum placeat modi facilis reprehenderit voluptates, doloribus unde saepe cumque est! Optio, quo? Illum, eos ea similique, laboriosam eaque odit voluptatum, rerum adipisci dolor vitae officiis quidem totam quisquam quae? Debitis enim eum fugit quae cupiditate quisquam vitae. Mollitia assumenda tenetur laudantium voluptate saepe facere ullam recusandae porro. Eos veniam eum, numquam iure praesentium suscipit nemo dolorem eveniet voluptatum, voluptas officia! Repellat, culpa deserunt. Fugiat, nostrum ut qui molestiae deleniti similique dignissimos modi dolorum, quod cupiditate nulla blanditiis eius perspiciatis aut repudiandae excepturi ab ea doloremque inventore quis ex facilis sunt libero optio. Minus totam dolor iusto necessitatibus repellat perferendis, eligendi, reprehenderit tempora in nulla ullam, aspernatur laudantium. Quam, excepturi explicabo? Aperiam suscipit dolore, ipsam enim architecto recusandae, voluptate totam nesciunt officia tempore dolorem. Fugiat, doloremque magnam? Debitis eos sint odit laboriosam, impedit expedita alias libero consectetur repellat animi sunt necessitatibus voluptatum repudiandae explicabo porro sapiente recusandae velit? Excepturi accusamus, quas possimus deserunt iste placeat nostrum voluptatibus illo! Placeat laboriosam porro tenetur inventore quo architecto sit quibusdam ea unde nulla quis eveniet, soluta reprehenderit ex perferendis numquam omnis sapiente corrupti temporibus quam tempore quos atque? Quae tenetur corrupti excepturi omnis quis ut sunt cupiditate at aspernatur perspiciatis inventore exercitationem officia, animi eligendi est commodi repudiandae minus officiis maiores doloremque dolore labore fugiat? Praesentium rerum, molestias aliquid alias maiores fugit, reiciendis quae ut fuga sunt illum doloribus? Quibusdam, provident nihil esse tempore harum neque ut unde, dicta ducimus obcaecati, voluptatum odio voluptas aut labore? Non, dolore nobis! Inventore itaque unde, dignissimos, delectus vero ex totam reprehenderit a in eos quisquam. Totam blanditiis earum neque exercitationem, nemo asperiores eum harum corrupti, reiciendis quibusdam, distinctio autem nihil. Labore incidunt ratione asperiores id quod. Modi voluptas aliquam veritatis exercitationem soluta! Ducimus amet necessitatibus, molestias a alias sequi. Maiores inventore doloremque minima explicabo nisi quisquam qui. Neque nemo, similique ad repellendus architecto provident illum itaque impedit explicabo tempora sequi rerum tempore alias, quae id dolor quam totam laudantium soluta magni eveniet ullam deserunt odit consectetur. Ratione, rerum architecto aut exercitationem ipsa velit quisquam, molestiae rem minus accusantium modi laboriosam, fugiat ex suscipit ea autem. Vero, blanditiis aperiam! Consequuntur cupiditate, voluptatum illo voluptatibus pariatur quibusdam facilis cum consectetur recusandae exercitationem tenetur maxime odit voluptates neque, deserunt fugiat nulla. Qui ipsa corrupti assumenda possimus debitis unde voluptatem corporis quae accusantium? Mollitia laudantium accusantium voluptates eveniet reiciendis, quae doloribus quod, dolorem similique magnam aperiam illum quidem nemo dolor quibusdam sed corrupti impedit dicta neque. Consequatur error consequuntur iusto id vel, porro incidunt praesentium ipsam rem nostrum sunt facere iste dolorem est quisquam ut delectus debitis minima laborum distinctio consectetur. Libero culpa ipsa deleniti possimus earum animi fugit, veritatis tempora laboriosam porro aspernatur ut doloremque quam, quae ipsam officia molestias placeat labore aliquid.</div>
         </section>
     </div>
   )

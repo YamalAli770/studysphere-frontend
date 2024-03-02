9cab8bc94e2fc5c9234a71aa0cbe1fe6bf9ceb26
001osama <osamanasim909@gmail.com>
2024-03-02 21:34:11 +0500

    getting minimum data from message creation

--- actions/chat.ts
+++ actions/chat.ts
@@ -1,21 +1,33 @@
 "use server";
 
 import { db } from "@/lib/db";
+import { currentUserServer } from "@/lib/user-server";
 import { MessageSchema } from "@/schemas";
 import * as z from "zod";
 
 export const sendMessage = async (values: z.infer<typeof MessageSchema>) => {
     try {
+      const user = await currentUserServer();
+
+      if(!user) {
+        return { error: "User not found!" }
+      };
+
+      if(user.id !== values.senderId) {
+          return { error: "User not authorized!" }
+      }
+      const validatedFields = MessageSchema.safeParse(values);
+
+      if(!validatedFields.success) {
+          return { error: "Invalid fields!" }
+      };
+
       const newMessage = await db.message.create({
         data: {
           content: values.content,
           senderId: values.senderId,
           conversationId: values.conversationId
-        },
-        include: {
-          sender: true,
-          conversation: true,
-        },
+        }
       });
   
       return newMessage;

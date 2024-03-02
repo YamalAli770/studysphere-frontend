20db13ba00fec4668e4a9d94062f5b4de31382f9
001osama <osamanasim909@gmail.com>
2024-03-02 21:35:58 +0500

    getting interface from separate file

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+import { ConversationWithExtras } from "@/types/conversation";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
@@ -6,19 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 
-
-interface Conversation {
-  id: string;
-  status: boolean;
-  lastMessage:string;
-  user_oneId:string;
-  user_one : {id:string, name:string, imageUrl:string};      
-  user_twoId:string;
-  user_two : {id:string, name:string, imageUrl:string};     
-  messages: Array<{ id:string, content:string, senderId: string, conversationId:string }>; 
-};
-
-export const getOtherUser = (conversation: Conversation, currentUserId: string | undefined ) => {
+export const getOtherUser = (conversation: ConversationWithExtras, currentUserId: string | undefined ) => {
   if (currentUserId) {
     if (conversation.user_one.id === currentUserId) {
       return conversation.user_two;

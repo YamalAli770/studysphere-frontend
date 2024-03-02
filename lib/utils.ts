f67fd1247be2ec35eebd2441b4fdb5c8fd7c7cc3
001osama <osamanasim909@gmail.com>
2024-03-02 10:51:12 +0500

    utility funtion for getting other user

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+
+interface Conversation {
+  id: string;
+  status: boolean;
+  lastMessage:string;
+  user_oneId:string;
+  user_one : {id:string, name:string, imageUrl:string};      
+  user_twoId:string;
+  user_two : {id:string, name:string, imageUrl:string};     
+  messages: Array<{ id:string, content:string, senderId: string, conversationId:string }>; 
+};
+
+export const getOtherUser = (conversation: Conversation, currentUserId: string | undefined ) => {
+  if (currentUserId) {
+    if (conversation.user_one.id === currentUserId) {
+      return conversation.user_two;
+    } else if (conversation.user_two.id === currentUserId) {
+      return conversation.user_one;
+    }
+  }
+  return null;
+};

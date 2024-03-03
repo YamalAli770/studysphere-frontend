23ac9de53233b7b3655eb1a65844365acfee1829
001osama <osamanasim909@gmail.com>
2024-03-03 13:33:10 +0500

    implemented pusher in server

--- actions/chat.ts
+++ actions/chat.ts
@@ -7,6 +7,7 @@ import * as z from "zod";
 
 export const sendMessage = async (values: z.infer<typeof MessageSchema>) => {
     try {
+      const Pusher = require("pusher");
       const user = await currentUserServer();
 
       if(!user) {
@@ -28,6 +29,18 @@ export const sendMessage = async (values: z.infer<typeof MessageSchema>) => {
           senderId: values.senderId,
           conversationId: values.conversationId
         }
+      });
+
+      const pusher = new Pusher({
+        appId: "1765277",
+        key: "85cc5781a9fb5d38f00f",
+        secret: "0f323451f3d1c69b7761",
+        cluster: "ap2",
+        useTLS: true
+      });
+      console.log("Pusher from server",pusher);
+      pusher.trigger("convId", "chat", {
+        message: `${JSON.stringify(newMessage)}\n\n`
       });
   
       return newMessage;

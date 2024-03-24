b4e34b702a43f36ae8897ac904a235e0cc780ed6
001osama <osamanasim909@gmail.com>
2024-03-24 13:10:31 +0500

    meetup page

--- app/(routes)/meetup/page.tsx
+++ app/(routes)/meetup/page.tsx
@@ -1,17 +1,9 @@
-'use client'
-import ConnectionForm from "./_components/connection-form";
+import Meeting from "./_components/meeting";
 
 const MeetUp = () => {  
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <div className="p-[3rem] border shadow-lg rounded-md">
-        <h2 className="mb-6 text-3xl font-bold text-center">
-          Connect to your <span className="text-accent-text">mentors</span> Worldwide!
-        </h2>
-        <ConnectionForm />
-      </div>
-    </main>
-  );
+   <Meeting/>
+  )
 };
 
 export default MeetUp;

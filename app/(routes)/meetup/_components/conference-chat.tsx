11e9f9d68c73ed34fb2a5824b4da168198dddde3
001osama <osamanasim909@gmail.com>
2024-03-24 13:06:19 +0500

    conference chat created

---
+++ app/(routes)/meetup/_components/conference-chat.tsx
@@ -0,0 +1,88 @@
+'use client';
+import { selectPeers, useHMSStore, selectHMSMessages,useHMSActions,selectLocalPeer  } from "@100mslive/react-sdk";
+import React, {useRef,useEffect} from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { 
+  Send as SendIcon
+} from 'lucide-react';
+
+interface ConferenceChatProps{
+    isChatOpen:Boolean;
+}
+
+const ConferenceChat =({isChatOpen}:ConferenceChatProps)=>{
+    const localPeer = useHMSStore(selectLocalPeer);
+  const hmsActions = useHMSActions();
+  const [newMessage, setNewMessage] = React.useState("");
+  const allMessages = useHMSStore(selectHMSMessages);
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    hmsActions.sendBroadcastMessage(newMessage);
+    setNewMessage("");
+  };
+
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [allMessages]);
+
+ 
+    return(
+        <div className={`${isChatOpen ? "w-2/6" : "w-0 hidden"} transition-all duration-100 ease-out flex flex-col gap-4 p-4 rounded-xl bg-[#2b2d2e]`}>
+          <div className="grow-0 p-4 w-full text-center rounded-md bg-dark-bg">
+            <div className="text-white">Conversations</div>
+          </div>
+          <div className="grow min-h-0 min-w-0">
+            <ScrollArea className="h-full w-full rounded-md">
+                <div>
+                  {allMessages.map((msg) =>{
+                  console.log(msg);
+                  console.log(localPeer);
+                   return(
+                    <div className={`w-full ${(msg.sender == localPeer?.id) ? "flex justify-end":"" }`} key={msg.id}>
+                      <div
+                      className={`flex flex-col ${(msg.sender == localPeer?.id) ? "bg-white/10":"bg-dark-bg"} w-fit p-2 mb-4 gap-1 rounded-md`}
+                      >
+                        <div className="flex gap-10 text-sm text-white opacity-50 justify-between">
+                          <span className="">
+                            {msg.senderName}
+                          </span>
+                          <span className="">
+                            {msg.time.getHours().toString().padStart(2, '0')}:{msg.time.getMinutes().toString().padStart(2, '0')}
+                          </span>
+                        </div>
+                        <span className="text-white break-all">{msg.message}</span>
+                      </div>
+                    </div>
+                  )})}
+                </div>
+                <div ref={messagesEndRef} />
+            </ScrollArea>
+          </div>
+          <div className="grow-0">
+            <form onSubmit={sendMessage} className="flex w-full items-center space-x-2">
+              <Input 
+              type="text"
+              value={newMessage}
+              onChange={(e)=>{setNewMessage(e.target.value)}} 
+              className="bg-dark-bg text-white" 
+              placeholder="Write a message..." />
+              <Button type="submit" className="bg-ternary-bg">
+                <SendIcon/>
+              </Button>
+            </form>
+          </div>
+        </div>
+    )
+}
+
+export default ConferenceChat;

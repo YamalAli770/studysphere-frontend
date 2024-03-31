ea3cefed7f7b96df3096a8ebe38c87753bce1552
001osama <osamanasim909@gmail.com>
2024-03-31 13:10:06 +0500

    prejoin implemented

--- app/(routes)/meetup/_components/join-form.tsx
+++ app/(routes)/meetup/_components/join-form.tsx
@@ -1,53 +1,70 @@
 'use client';
-import React, { useState, useEffect, FormEvent } from "react";
-import { useHMSActions } from "@100mslive/react-sdk";
+import React, { useState, useEffect, useRef, FormEvent } from "react";
+import { useHMSActions, useHMSStore, selectLocalPeer } from "@100mslive/react-sdk";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { getUserById } from "@/lib/data/user";
-import { currentUser } from "@/lib/auth";
+import PreJoin from "./prejoin";
+
 
 const JoinForm: React.FC = () => {
   const hmsActions = useHMSActions();
   const [roomName, setRoomName] = useState<string>("");
+  const [authToken, setAuthToken] = useState<string | null>(null);
   const userName="Osama Naseem";
+  
   const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
     const roomCode = roomName;
 
     // use room code to fetch auth token
     const authToken = await hmsActions.getAuthTokenByRoomCode({ roomCode });
+    setAuthToken(authToken);
 
-    try {
-      await hmsActions.join({ userName: userName, authToken });
-    } catch (e) {
-      console.error(e);
-    }
+    const config = {
+      userName: userName,
+      authToken: authToken, // client-side token generated from your token service
+      settings: {
+          // initial states
+          isAudioMuted: false,
+          isVideoMuted: false
+      },
+      rememberDeviceSelection: true, // remember manual device change
+      captureNetworkQualityInPreview: false // whether to measure network score in preview
+    };
+    await hmsActions.preview(config);
+    
   };
 
   return (
-
-    <div className="h-screen flex justify-center items-center">
-      <div className="w-4/5 md:w-3/6 lg:w-2/6 p-8 text-center rounded-md shadow-lg border">
-        <h1 className="text-2xl text-primary-text font-extrabold mb-8">Join Room</h1>
-        <div className="flex gap-4">
-          <Button>Create Room</Button>
-          <hr className="border-l h-10"/>
-          <form className="flex gap-3" onSubmit={handleSubmit}>
-            <Input
-              className="mb-4"
-              id="room-code"
-              type="text"
-              name="roomCode"
-              placeholder="Room code"
-              required
-              onChange={(e) => setRoomName(e.target.value)}
-            />
-            <Button type="submit">
-              Join Room
-            </Button>
-          </form>
+    <div className={`h-screen ${authToken ? "bg-dark-bg":""} flex justify-center items-center`}>
+      {authToken ? 
+      (
+        <div className="w-4/5 md:w-3/6 lg:w-2/6">
+          <PreJoin authToken={authToken} username={userName} />
         </div>
-      </div>
+      ):(
+        <div className="w-4/5 md:w-3/6 lg:w-2/6 p-8 text-center rounded-md shadow-lg border">
+          <h1 className="text-2xl text-primary-text font-extrabold mb-8">Join Room</h1>
+          <div className="flex gap-4">
+            <Button>Create Room</Button>
+            <hr className="border-l h-10"/>
+            <form className="flex gap-3" onSubmit={handleSubmit}>
+              <Input
+                className="mb-4"
+                id="room-code"
+                type="text"
+                name="roomCode"
+                placeholder="Room code"
+                required
+                onChange={(e) => setRoomName(e.target.value)}
+              />
+              <Button type="submit">
+                Join Room
+              </Button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

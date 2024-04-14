e943ab15b4c5a7fa9726caaa3b5fc0ac773b2ee0
001osama <osamanasim909@gmail.com>
2024-04-14 18:01:39 +0500

    conference ui fixed minor issues remains

--- app/(routes)/meetup/_components/conference.tsx
+++ app/(routes)/meetup/_components/conference.tsx
@@ -27,9 +27,10 @@ function Conference() {
   const isScreenSharing = useHMSStore(selectIsSomeoneScreenSharing);
   const peerSharingScreen = useHMSStore(selectPeersScreenSharing);
 
-  const [initialTime] = useState(new Date().toISOString()); // Current time
-  const [endTime] = useState(new Date(Date.now() + 30 * 60 * 1000).toISOString()); // 20 minutes from now
-
+  const [startTime] = useState(new Date(Date.now() + 1 * 60 * 1000).toISOString()); // Current time + 1 min
+  const [endTime] = useState(new Date(Date.now() + 30 * 60 * 1000).toISOString()); // 30 minutes from now
+  const [meetingTime] =useState(new Date(Date.now() + 1 * 60 * 1000).toLocaleTimeString());
+  console.log(startTime);
   const room = useHMSStore((store)=>store.room);
   console.log(room);
   const endRoom = async () => {
@@ -51,19 +52,18 @@ function Conference() {
   }
   return (
     <div className="h-screen max-h-screen max-w-screen box-border flex flex-col bg-dark-bg relative">
-      <div className="flex justify-between items-center cursor-default grow-0 gap-4 border-b border-[#2b2d2e]">
-        <div className="flex gap-4 p-4 text-white">
+      <div className="flex px-4 py-3 justify-between items-center cursor-default grow-0 gap-4 border-b border-[#2b2d2e]">
+        <div className="flex gap-4 text-white">
           <span className="flex gap-2 font-bold text-destructive animate-pulse duration-400">
             <RecordingIcon/>
             <span>REC</span>
           </span>
           <hr className="border-l border-[#2b2d2e] h-6"/>
-          <ConferenceTimer initialTime={initialTime} endTime={endTime} meetEnd={endRoom}/>
+          <div className="flex gap-3 rounded text-white">
+            <div className="">Meeting start time {meetingTime}</div>
+          </div>
         </div>
-        <div className="flex gap-3 p-1 me-4 rounded py-1 px-3 text-white">
-          <div className="select-none">hyv-smgc-ort</div>
-          <button className="p-[6px] rounded border border-white/20 hover:bg-white/20"><CopyIcon size={12}/></button>
-        </div>
+        <ConferenceTimer startTime={startTime} endTime={endTime} meetEnd={endRoom}/>
       </div>
       <div className="grow min-h-0 flex p-4 gap-4">
         <div className={`flex flex-col gap-4 duration-100 ease-out ${isChatOpen ? "w-4/6" : "w-full"}`}>

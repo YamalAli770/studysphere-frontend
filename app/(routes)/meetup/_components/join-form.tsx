d4d16a2969f2193b59465d851e6577ebc4cae1f7
001osama <osamanasim909@gmail.com>
2024-04-14 17:59:37 +0500

    join form modified

--- app/(routes)/meetup/_components/join-form.tsx
+++ app/(routes)/meetup/_components/join-form.tsx
@@ -4,6 +4,7 @@ import { useHMSActions, useHMSStore, selectLocalPeer } from "@100mslive/react-sd
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import PreJoin from "./prejoin";
+import {sendConfirmationMail} from "@/lib/node-mailer";
 
 
 const JoinForm: React.FC = () => {
@@ -11,6 +12,35 @@ const JoinForm: React.FC = () => {
   const [roomName, setRoomName] = useState<string>("");
   const [authToken, setAuthToken] = useState<string | null>(null);
   const userName="Osama Naseem";
+
+  const handleSendMail = async ()=> {
+    try{
+      // "use server";
+      // await sendConfirmationMail({menteeEmail:"osamanasim909@gmail.com",menteeName:"Osama",orderNo:"123456"});
+      console.log("room create button pressed");
+      // const data = {
+      //   name:"osama",
+      //   id:"304"
+      // }
+      
+      // const res = await fetch(`/api/email/schedule-email`,
+      // {
+      //   method: 'POST',
+      //   body: JSON.stringify(data),
+      //   headers: {
+      //     'content-type':'application/json'
+      //   }
+      // }
+      // )
+    }
+    catch (e) {
+      if (e instanceof Error) {
+          console.error("Error:", e.message);
+      } else {
+          console.error("An unknown error occurred");
+      }
+    }
+  }
   
   const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
@@ -46,7 +76,7 @@ const JoinForm: React.FC = () => {
         <div className="w-4/5 md:w-3/6 lg:w-2/6 p-8 text-center rounded-md shadow-lg border">
           <h1 className="text-2xl text-primary-text font-extrabold mb-8">Join Room</h1>
           <div className="flex gap-4">
-            <Button>Create Room</Button>
+            <Button onClick={handleSendMail}>Create Room</Button>
             <hr className="border-l h-10"/>
             <form className="flex gap-3" onSubmit={handleSubmit}>
               <Input

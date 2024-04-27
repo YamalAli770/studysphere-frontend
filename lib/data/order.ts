84a67261b529e970df090fc2d0659678be184302
001osama <osamanasim909@gmail.com>
2024-04-27 16:14:47 +0500

    rename method

--- lib/data/order.ts
+++ lib/data/order.ts
@@ -2,7 +2,7 @@
 
 import { db } from "../db"
 
-export const OrderWithMeetingReq = async (roomId:string) => {
+export const MeetupRequestFromRoomId = async (roomId:string) => {
     const orderWithExtras = await db.order.findFirst({
         where:{
             roomId: roomId

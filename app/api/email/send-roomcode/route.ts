0a9757e44f43ed5c41bf94693843831da37cc98b
001osama <osamanasim909@gmail.com>
2024-04-27 16:18:01 +0500

    email functionality made dynamic

--- app/api/email/send-roomcode/route.ts
+++ app/api/email/send-roomcode/route.ts
@@ -1,4 +1,5 @@
 
+import { activateRoom } from "@/lib/hms-server";
 import { sendMail, sendRoomCodeMail } from "@/lib/node-mailer";
 import { verifySignatureAppRouter } from "@upstash/qstash/dist/nextjs";
 import { NextRequest,NextResponse } from "next/server";
@@ -7,13 +8,13 @@ async function handler(request: NextRequest) {
     
     try {
         const data = await request.json();
-        const menteeCode="123-123-123";
-        const mentorCode="456-456-456";
-        console.log("API Hitting");
-        console.log(data);
+        
+        const roomCode = await activateRoom(data.roomId);
+        const menteeCode = roomCode.filter(room => room.role === "mentee").map(room => room.code);
+        const mentorCode = roomCode.filter(room => room.role === "mentor").map(room => room.code);
+        
         await sendRoomCodeMail({...data, menteeCode, mentorCode })
-        // const { id, name } = data.body;
-        // console.log(`Email API Working fine id is ${id} and name is ${name}`);
+
         return NextResponse.json(
             { message: "Success" }
         )

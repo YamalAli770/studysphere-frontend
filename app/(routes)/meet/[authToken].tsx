b26f8237cf45c53bc4b4a87aa04a344726d2145a
001osama <osamanasim909@gmail.com>
2024-04-28 19:39:52 +0500

    removed unnecessary files

--- app/(routes)/meet/[authToken].tsx
+++
@@ -1,9 +0,0 @@
-import Meeting from "./_components/meeting";
-import { useRouter } from 'next/router'
-export default function Page() {  
-  const router = useRouter()
-  return (
-  //  <Meeting/>
-  <p>Post: {router.query.authToken}</p>
-  )
-};

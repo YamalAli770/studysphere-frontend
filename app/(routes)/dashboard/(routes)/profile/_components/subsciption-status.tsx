2a24395831f9f9ff8a49bcd1b56a60d51f36dd64
001osama <osamanasim909@gmail.com>
2024-05-06 21:32:10 +0500

    subscription status viewing component created

---
+++ app/(routes)/dashboard/(routes)/profile/_components/subsciption-status.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { Button } from "@/components/ui/button";
+interface SubscriptionInfo {
+    status:string;
+    plan:string;
+    meetings:number|null;
+};
+
+interface SubscriptionStatusProps{
+    subscriptionInfo:SubscriptionInfo | null | undefined
+}
+
+export default function SubscriptionStatus({subscriptionInfo}:SubscriptionStatusProps){
+    const router = useRouter();
+    console.log(subscriptionInfo);
+    return(
+        <div>
+            {subscriptionInfo != null || subscriptionInfo != undefined ? 
+            (
+            <div className="flex flex-col bg-gray-200 rounded-lg p-2 shadow-md text-xs font-semibold">
+                <div>Remaining meetings: {subscriptionInfo?.meetings}</div>
+                <div>Plan: {subscriptionInfo?.plan}</div>
+                <div>Status: {subscriptionInfo?.status}</div>
+            </div>
+            )
+            :
+            <Button onClick={()=>router.push('/subscription')}>Get subscription</Button>}
+        </div>
+        
+    )
+}
